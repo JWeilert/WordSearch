f05cfcbd4bfab1784d7dc9ac6acb57121551c70f
John Weilert <mattweilert@gmail.com>
2022-09-23 16:25:18 -0500

    Added more style and an option to play audio if availible

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Lookup from "./components/LookUp";
 function App() {
   return (
     <div className="App">
+      <h1>Search Any Word!</h1>
+      <hr />
       <Lookup />
     </div>
   );
--- src/components/LookUp.js
+++ src/components/LookUp.js
@@ -6,6 +6,8 @@ function Lookup() {
   var [word, setWord] = useState({});
   var [definition, setDefinition] = useState(null);
   var [definitionNumber, setDefinitionNumber] = useState(0);
+  var [audio, setAudio] = useState("");
+
   function loadSearch() {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordSearch}`)
       .then((response) => response.json())
@@ -21,12 +23,26 @@ function Lookup() {
         setDefinition(
           "We couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead."
         )
-      );
+      )
+      .then(function () {
+        setAud();
+      });
   }
 
   function setText() {
-    console.log(word.length);
     setDefinition(word[0].meanings[0].definitions[definitionNumber].definition);
+  }
+
+  function setAud() {
+    setAudio("");
+    audio = word[0].phonetics[0].audio;
+    setAudio(audio);
+    console.log(audio);
+  }
+
+  function playAud() {
+    let playAudio = new Audio(audio);
+    playAudio.play();
   }
 
   function nextDefinition() {
@@ -37,46 +53,77 @@ function Lookup() {
     }
   }
 
+  function prevDefinition() {
+    if (definitionNumber > 0) {
+      setDefinitionNumber((definitionNumber -= 1));
+    } else {
+      setDefinitionNumber(word[0].meanings[0].definitions.length - 1);
+    }
+  }
+
   useEffect(() => {
     loadSearch();
-  }, [definitionNumber, wordSearch, setWord]);
+  }, [definitionNumber, wordSearch, setWord, audio]);
 
   return (
     <div>
-      <div className="box">
-        <div id="searchedWord">
-          {wordSearch != "" && (
-            <h1>{wordSearch[0].toUpperCase() + wordSearch.substring(1)}</h1>
-          )}{" "}
+      <form>
+        <lable>
+          <input
+            type="text"
+            placeholder="Enter Word..."
+            onChange={(e) => {
+              setDefinitionNumber(0);
+              setWordSearch(e.target.value);
+            }}
+          />
+        </lable>
+      </form>
+      <div id="flex">
+        <div className="buttonContainer">
+          <button className="defButton" onClick={prevDefinition}>
+            Prev Definition
+          </button>
+        </div>
+        <div className="box">
+          <div id="searchedWord">
+            {wordSearch !== "" && (
+              <h1>{wordSearch[0].toUpperCase() + wordSearch.substring(1)}</h1>
+            )}{" "}
+            <hr />
+          </div>
+          <div id="definition">
+            <h2>{definition}</h2>
+          </div>
+          <div id="outOf">
+            {word.length !== undefined && (
+              <out>
+                Definition: {definitionNumber + 1} of{" "}
+                {word[0].meanings[0].definitions.length}
+              </out>
+            )}
+          </div>
+          <br />
           <hr />
-        </div>
-        <div id="definition">
-          <h2>{definition}</h2>
-        </div>
-        <div id="outOf">
-          {word.length != undefined && (
-            <out>
-              Definition: {definitionNumber + 1} of{" "}
-              {word[0].meanings[0].definitions.length}
-            </out>
+          {audio !== "" && word.length !== undefined && (
+            <div>
+              <button
+                onClick={() => {
+                  setAud();
+                  playAud();
+                }}
+              >
+                Play Audio
+              </button>
+            </div>
           )}
         </div>
+        <div className="buttonContainer">
+          <button className="defButton" onClick={nextDefinition}>
+            Next Definition
+          </button>
+        </div>
       </div>
-      <div>
-        <p>Enter Word:</p>
-        <form>
-          <lable>
-            <input
-              type="text"
-              onChange={(e) => {
-                setDefinitionNumber(0);
-                setWordSearch(e.target.value);
-              }}
-            />
-          </lable>
-        </form>
-      </div>
-      <button onClick={nextDefinition}>Next Definition</button>
     </div>
   );
 }
